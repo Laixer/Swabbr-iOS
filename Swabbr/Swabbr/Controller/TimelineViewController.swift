9c741ce4165d092735c158a89be1fb0768b4d1e0
James <jamesb@live.nl>
2019-09-18 16:09:03 +0200

    added timelineviewcontroller

---
+++ Swabbr/Swabbr/Controller/TimelineViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TimelineViewController.swift
+//  Swabbr
+//
+//  Created by Anonymous on 18-09-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController : UIViewController {
+    
+    override func viewDidLoad() {
+    }
+    
+}
