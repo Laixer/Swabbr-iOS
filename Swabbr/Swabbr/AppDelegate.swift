5d68cbeb53203f3fd7704ab52b03da8d7eb03864
James <jamesb@live.nl>
2019-09-18 12:52:20 +0200

    removed navigation controller code, using storyboard

--- Swabbr/Swabbr/AppDelegate.swift
+++ Swabbr/Swabbr/AppDelegate.swift
@@ -15,15 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        // setting up navigation controller
-        let viewController = ViewController(nibName: nil, bundle: nil) //ViewController = Name of your controller
-        let navigationController = UINavigationController(rootViewController: viewController)
-        
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = navigationController
-        self.window?.makeKeyAndVisible()
-        
         return true
     }
 
