6281ec67d7a0fcfe44c7ae85d9c2b4b4a7abdebd
James <jamesb@live.nl>
2019-09-24 10:58:22 +0200

    base video struct

---
+++ Swabbr/Swabbr/Struct/Video.swift
@@ -0,0 +1,27 @@
+//
+//  Video.swift
+//  Swabbr
+//
+//  Created by Anonymous on 19-09-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var uid: String
+    var views: Int
+    var likes: Int
+    var owner: User
+    
+    init(uid: String, views: Int, likes: Int, owner: User) {
+        self.uid = uid
+        self.views = views
+        self.likes = likes
+        self.owner = owner
+    }
+    
+    // add video related methods like "like" and "react"
+    
+}
