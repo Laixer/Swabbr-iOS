0456f91e718b045f57aa7ea80ed3952a35953a44
James <jamesb@live.nl>
2019-09-26 10:16:44 +0200

    makes it conform to api response - changes required

--- Swabbr/Swabbr/Struct/User.swift
+++ Swabbr/Swabbr/Struct/User.swift
@@ -5,22 +5,27 @@
 //  Created by James Bal on 18-09-19.
 //  Copyright © 2019 Laixer. All rights reserved.
 //
-// Base class of an user
+//  Base class of an user
 
 import Foundation
 
-struct User {
+struct User: Decodable {
     
-    var uid: Int64
+    var id: String
+    var firstName: String
+    var lastName: String
+    var gender: String
+    var country: String
+    var email: String
+    var birthdate: String
+    var timezone: String
     var username: String
-    var profileImage: Int64
-    var followersCount: Int
-    
-    init(uid: Int64, username: String, profileImage: Int64, followersCount: Int) {
-        self.uid = uid
-        self.username = username
-        self.profileImage = profileImage
-        self.followersCount = followersCount
-    }
+    var profileImageUrl: String
+    var interests: [String]
+    var totalVlogs: Int
+    var totalFollowers: Int
+    var totalFollowing: Int
+    var longitude: String
+    var latitude: String
     
 }
