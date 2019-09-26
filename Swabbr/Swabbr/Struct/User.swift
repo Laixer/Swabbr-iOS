247b976daeaeebf74fa0160f5ae7e559c5d8f526
James <jamesb@live.nl>
2019-09-26 14:12:41 +0200

    updated user to fit remote to model

--- Swabbr/Swabbr/Struct/User.swift
+++ Swabbr/Swabbr/Struct/User.swift
@@ -11,13 +11,13 @@ import Foundation
 
 struct User: Decodable {
     
-    var id: String
+    var id: Int
     var firstName: String
     var lastName: String
-    var gender: String
+    var gender: Gender
     var country: String
     var email: String
-    var birthdate: String
+    var birthdate: Date
     var timezone: String
     var username: String
     var profileImageUrl: String
@@ -25,7 +25,76 @@ struct User: Decodable {
     var totalVlogs: Int
     var totalFollowers: Int
     var totalFollowing: Int
-    var longitude: String
-    var latitude: String
+    var longitude: Float
+    var latitude: Float
+    
+    /**
+     Converts the value of gender to conform our model.
+    */
+    enum Gender: String, Decodable {
+        case Male = "M"
+        case Female = "F"
+    }
+    
+    /**
+     Handles possible name convention differences.
+     Put each value in their respected model variant.
+    */
+    enum CodingKeys: String, CodingKey {
+        case id, firstName, lastName, gender, country, email, birthdate, timezone, profileImageUrl, interests, totalVlogs, totalFollowers, totalFollowing, longitude, latitude
+        case username = "nickname"
+    }
+    
+    /**
+     This function makes the data confirm to the model.
+     It will try and parse the values to their correct value according to the model.
+     - parameter decoder: The decoder built in swift to read the data from.
+     - Throws: A decodingerror when the data can't be converted to their respective type.
+    */
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let idString = try container.decode(String.self, forKey: CodingKeys.id)
+        guard let idInt = Int(idString) else {
+            let context = DecodingError.Context(codingPath: container.codingPath + [CodingKeys.id], debugDescription: "Could not parse json key to Int object")
+            throw DecodingError.dataCorrupted(context)
+        }
+        id = idInt
+        
+        firstName = try container.decode(String.self, forKey: CodingKeys.firstName)
+        lastName = try container.decode(String.self, forKey: CodingKeys.lastName)
+        gender = try container.decode(Gender.self, forKey: CodingKeys.gender)
+        country = try container.decode(String.self, forKey: CodingKeys.country)
+        email = try container.decode(String.self, forKey: CodingKeys.email)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC+0:00")
+        let birthdateString = try container.decode(String.self, forKey: CodingKeys.birthdate)
+        birthdate = dateFormatter.date(from: birthdateString)!
+        
+        timezone = try container.decode(String.self, forKey: CodingKeys.timezone)
+        username = try container.decode(String.self, forKey: CodingKeys.username)
+        profileImageUrl = try container.decode(String.self, forKey: CodingKeys.profileImageUrl)
+        interests = try container.decode([String].self, forKey: CodingKeys.interests)
+        totalVlogs = try container.decode(Int.self, forKey: CodingKeys.totalVlogs)
+        totalFollowers = try container.decode(Int.self, forKey: CodingKeys.totalFollowers)
+        totalFollowing = try container.decode(Int.self, forKey: CodingKeys.totalFollowing)
+        
+        let longitudeString = try container.decode(String.self, forKey: CodingKeys.longitude)
+        guard let longitudeFloat = Float(longitudeString) else {
+            let context = DecodingError.Context(codingPath: container.codingPath + [CodingKeys.longitude], debugDescription: "Could not parse json key to Float object")
+            throw DecodingError.dataCorrupted(context)
+        }
+        longitude = longitudeFloat
+        
+        let latitudeString = try container.decode(String.self, forKey: CodingKeys.latitude)
+        guard let latitudeFloat = Float(latitudeString) else {
+            let context = DecodingError.Context(codingPath: container.codingPath + [CodingKeys.latitude], debugDescription: "Could not parse json key to Float object")
+            throw DecodingError.dataCorrupted(context)
+        }
+        latitude = latitudeFloat
+        
+    }
     
 }
