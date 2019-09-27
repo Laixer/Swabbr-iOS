27f1913c20672881779d3586650093f6e29835fb
James <jamesb@live.nl>
2019-09-27 13:59:16 +0200

    updated to a pageview

--- Swabbr/Swabbr/Controller/TimelineViewController.swift
+++ Swabbr/Swabbr/Controller/TimelineViewController.swift
@@ -8,10 +8,94 @@
 
 import UIKit
 
-class TimelineViewController : UIViewController {
+class TimelineViewController : UIPageViewController {
     
-    private let user = User(uid: 1, username: "James", profileImage: 32434543654, followersCount: 23)
+    private var tableView: UITableView!
+    internal var vlogViewControllers: [VlogPageViewController] = []
+    
+    /**
+     We use this function to override the default values to make it compatible with our needs.
+     The UIPageVIewController uses pageCurl as the default transitionStyle, we override this to the scroll type.
+     - parameter style: A TransitionStyle value.
+     - parameter navigationOrientation: A NavigationOrientation value.
+     - parameter options: A dictionary in the following format <OptionsKey : Any>.
+    */
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
+        
+        view.backgroundColor = UIColor.white
+        
+        title = "Timeline"
+        
+        dataSource = self
+        delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showCamera))
+        
+        let sData = ServerData()
+        sData.getUsers(onComplete: {_ in
+            sData.getVlogs(onComplete: {vlogs in
+                for vlog in vlogs! {
+                    self.vlogViewControllers.append(VlogPageViewController(vlog: vlog))
+                }
+                self.setViewControllers([self.vlogViewControllers.first!], direction: .forward, animated: true, completion: nil)
+            })
+        })
+        
     }
+    
+    @objc func showCamera() {
+        show(VlogStreamViewController(), sender: nil)
+    }
+}
+
+extension TimelineViewController : UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    // MARK: UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = vlogViewControllers.firstIndex(of: viewController as! VlogPageViewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return vlogViewControllers.last
+        }
+        
+        guard vlogViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return vlogViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = vlogViewControllers.firstIndex(of: viewController as! VlogPageViewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let vlogViewControllersCount = vlogViewControllers.count
+        
+        guard vlogViewControllersCount != nextIndex else {
+            return vlogViewControllers.first
+        }
+        
+        guard vlogViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return vlogViewControllers[nextIndex]
+    }
+    
+    
 }
