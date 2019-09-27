584cd47c459a9e3644697c45d5d585044e4131ac
James <jamesb@live.nl>
2019-09-27 13:59:51 +0200

    base for notification handling

---
+++ Swabbr/Swabbr/NotificationHandler.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationHandler.swift
+//  Swabbr
+//
+//  Created by James Bal on 20-09-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+class NotificationHandler {
+    
+    // function to send notification to server
+    func sendNotification() {
+        
+    }
+    
+    // function to handle incoming notification
+    func handleIncomingNotification() {
+        
+    }
+    
+}
