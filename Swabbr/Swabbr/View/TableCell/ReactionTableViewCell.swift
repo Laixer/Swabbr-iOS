05c161d5501d351b9bf200b69dea8e79b350a944
James <jamesb@live.nl>
2019-10-02 10:40:20 +0200

    temp reaction cell with base video

--- Swabbr/Swabbr/View/TableCell/ReactionTableViewCell.swift
+++ Swabbr/Swabbr/View/TableCell/ReactionTableViewCell.swift
@@ -7,25 +7,41 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ReactionTableViewCell : UITableViewCell {
     
     let userUsernameLabel = UILabel()
     let dateLabel = UILabel()
+    let player = AVPlayer(url: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!)
+    let playerView = AVPlayerViewController()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         // disable auto constraints to overwrite with own constraints
+        playerView.view.translatesAutoresizingMaskIntoConstraints = false
         userUsernameLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
         
+        // TODO: set dynamic video url
+        playerView.player = player
+        playerView.videoGravity = .resizeAspectFill
+        playerView.showsPlaybackControls = false
+        
         // add ui components to view
+        contentView.addSubview(playerView.view)
         contentView.addSubview(userUsernameLabel)
         contentView.addSubview(dateLabel)
         
         // apply constraints
         NSLayoutConstraint.activate([
+            
+            playerView.view.topAnchor.constraint(equalTo: contentView.topAnchor),
+            playerView.view.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            playerView.view.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            playerView.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             
             // userUsernameLabel
             userUsernameLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
