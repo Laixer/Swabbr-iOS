c4a083a91ffc4164f84e9124fd584e9e2386b186
James <jamesb@live.nl>
2019-10-02 10:40:33 +0200

    global current user

--- Swabbr/Swabbr/AppDelegate.swift
+++ Swabbr/Swabbr/AppDelegate.swift
@@ -12,7 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    // save currentuser
+    var currentUser: User?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // setup window
