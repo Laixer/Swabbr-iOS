acc1857d79c7c05f2d947f7b96ffcb6f10e170a4
James <jamesb@live.nl>
2019-10-04 16:04:41 +0200

    added memorycache dependency

--- Swabbr/Pods/Target Support Files/Pods-Swabbr/Pods-Swabbr-frameworks.sh
+++ Swabbr/Pods/Target Support Files/Pods-Swabbr/Pods-Swabbr-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MemoryCache/MemoryCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NextLevel/NextLevel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyCam/SwiftyCam.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MemoryCache/MemoryCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NextLevel/NextLevel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyCam/SwiftyCam.framework"
 fi
