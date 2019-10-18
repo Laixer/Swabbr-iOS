c6f28c531f241c6a052619fc99a53731d85e41b0
James <jamesb@live.nl>
2019-10-18 10:23:14 +0200

    bridging header for hmac encryption

---
+++ Swabbr/Swabbr/BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  Swabbr
+//
+//  Created by James Bal on 15-10-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
