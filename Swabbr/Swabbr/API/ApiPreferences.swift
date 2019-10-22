21c3a27d9037b0670b0e58287973cd5856c3c47f
James <jamesb@live.nl>
2019-10-22 14:25:01 +0200

    updated api preference handling

---
+++ Swabbr/Swabbr/API/ApiPreferences.swift
@@ -0,0 +1,35 @@
+//
+//  Preferences.swift
+//  Swabbr
+//
+//  Created by James Bal on 14-10-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+struct ApiPreferences: Decodable {
+    var api_url: String
+    var url_path: String
+}
+
+extension ApiPreferences {
+    
+    enum Enviroment: String {
+        case Test = "test"
+        case Release = "release"
+    }
+    
+    /**
+     Get the appropriate api data from the supporting plist file.
+     - parameter enviroment: A enviroment value.
+     - Returns: A preference containing an api url and url path.
+    */
+    static func getAPIPreferences(enviroment: Enviroment = .Release) -> ApiPreferences {
+        let url = Bundle.main.url(forResource: "API-Preferences", withExtension: "plist")!
+        let data = try! Data(contentsOf: url)
+        let preferences = try? PropertyListDecoder().decode(Dictionary<String, ApiPreferences>.self, from: data)
+        return preferences![enviroment.rawValue]!
+    }
+    
+}
