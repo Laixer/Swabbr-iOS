f223c84e0e2c4b975b0495f447415aac2fddfa11
James <jamesb@live.nl>
2019-10-24 12:10:54 +0200

    updated user tests

--- SwabbrTests/UserTests.swift
+++ SwabbrTests/UserTests.swift
@@ -10,27 +10,33 @@ import XCTest
 @testable import Swabbr
 
 class UserTests: XCTestCase {
+    
+    var jsonString: String!
+    var decoder: JSONDecoder!
 
     override func setUp() {
+        
+        jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"53.00003\"}"
+        decoder = JSONDecoder()
         
     }
 
     override func tearDown() {
         
+        jsonString = nil
+        decoder = nil
+        
     }
 
     func testJSONToUser() {
-        let jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"53.00003\"}"
         let jsonData = jsonString.data(using: .utf8)
-        let decoder = JSONDecoder()
         let user = try? decoder.decode(User.self, from: jsonData!)
         XCTAssertNotNil(user, "The json string does not conform the user model")
     }
     
     func testErrorMessageWhenConvertToFloatFails() {
-        let jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"Test\"}"
+        jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"Test\"}"
         let jsonData = jsonString.data(using: .utf8)
-        let decoder = JSONDecoder()
         XCTAssertThrowsError(try decoder.decode(User.self, from: jsonData!)) { (error) in
             print(error)
         }
@@ -38,12 +44,24 @@ class UserTests: XCTestCase {
     }
     
     func testForCorrectDate() {
-        let jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"53.00003\"}"
         let jsonData = jsonString.data(using: .utf8)
-        let decoder = JSONDecoder()
         let user = try? decoder.decode(User.self, from: jsonData!)
+        
+        let expectedDateString = "2014-06-02T00:00:00.000Z"
 
-        XCTAssert(user!.birthdate.iso8601() == "2014-06-02T00:00:00.000Z", "The user birthdate: \(user!.birthdate.iso8601()) is not the same as: 2014-06-02T00:00:00.000Z")
+        XCTAssert(user!.birthdate.iso8601() == expectedDateString, "The user birthdate: \(user!.birthdate.iso8601()) is not the same as: \(expectedDateString)")
+    }
+    
+    
+    func testPayloadWithUserObject() {
+        
+        let payloadString = "{\"protocol\":\"swabbr\",\"protocol_version\":1,\"data_type\":\"notification\",\"data_type_version\":1,\"data\":\(jsonString!),\"content_type\":\"json\",\"timestamp\":\"\",\"user_agent\":\"iphone\"}"
+        let jsonData = payloadString.data(using: .utf8)
+        let payloadWithUserObject = try? decoder.decode(Payload<User>.self, from: jsonData!)
+        
+        
+        XCTAssert((payloadWithUserObject!._protocol == "swabbr" && payloadWithUserObject!.innerData.gender == User.Gender.Male), "Payload contains wrong data")
+
     }
 
 }
