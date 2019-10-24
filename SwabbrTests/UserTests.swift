9f01f96556d351fc9786b9b62aa6c87e3fa6d565
James <jamesb@live.nl>
2019-10-24 14:08:48 +0200

    added test to check user object to json value

--- SwabbrTests/UserTests.swift
+++ SwabbrTests/UserTests.swift
@@ -16,7 +16,7 @@ class UserTests: XCTestCase {
 
     override func setUp() {
         
-        jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"53.00003\", \"latitude\": \"53.00003\"}"
+        jsonString = "{\"id\": \"0\", \"firstName\": \"Apple\", \"lastName\": \"Swift\", \"gender\": \"M\", \"country\": \"Germany\", \"email\": \"apple@swift.com\", \"birthdate\": \"02/06/2014\", \"timezone\": \"+2\", \"nickname\": \"AppleIsTheBest\", \"profileImageUrl\": \"image.png\", \"interests\": [\"Apple\", \"Swift\", \"Innovation\"], \"totalVlogs\": 2, \"totalFollowers\":6, \"totalFollowing\": 2014, \"longitude\": \"0.0\", \"latitude\": \"0.0\"}"
         decoder = JSONDecoder()
         
     }
@@ -52,13 +52,29 @@ class UserTests: XCTestCase {
         XCTAssert(user!.birthdate.iso8601() == expectedDateString, "The user birthdate: \(user!.birthdate.iso8601()) is not the same as: \(expectedDateString)")
     }
     
+    func testUserToJSON() {
+        
+        let jsonData = jsonString.data(using: .utf8)
+        let userObject = try? decoder.decode(User.self, from: jsonData!)
+        
+        let encoder = JSONEncoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        encoder.dateEncodingStrategy = .formatted(dateFormatter)
+        let userJsonBytes = try? encoder.encode(userObject)
+        let userJson = try? JSONSerialization.jsonObject(with: userJsonBytes!, options: []) as! [String: AnyHashable]
+        
+        let originalJson = try? JSONSerialization.jsonObject(with: jsonData!, options: []) as! [String: AnyHashable]
+
+        XCTAssertEqual(originalJson, userJson, "The original json is not equal to the user generated json: Original: \(originalJson) | Generated: \(userJson)")
+        
+    }
     
     func testPayloadWithUserObject() {
         
         let payloadString = "{\"protocol\":\"swabbr\",\"protocol_version\":1,\"data_type\":\"notification\",\"data_type_version\":1,\"data\":\(jsonString!),\"content_type\":\"json\",\"timestamp\":\"\",\"user_agent\":\"iphone\"}"
         let jsonData = payloadString.data(using: .utf8)
         let payloadWithUserObject = try? decoder.decode(Payload<User>.self, from: jsonData!)
-        
         
         XCTAssert((payloadWithUserObject!._protocol == "swabbr" && payloadWithUserObject!.innerData.gender == User.Gender.Male), "Payload contains wrong data")
 
