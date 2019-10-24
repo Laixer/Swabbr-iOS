4cbfe8d60d81371b731c1218ab61678930f90e5f
James <jamesb@live.nl>
2019-10-24 17:58:18 +0200

    updated vlog reactions tests

--- SwabbrTests/VlogReactionTests.swift
+++ SwabbrTests/VlogReactionTests.swift
@@ -10,21 +10,42 @@ import XCTest
 @testable import Swabbr
 
 class VlogReactionTests: XCTestCase {
+    
+    var jsonString: String!
+    var decoder: JSONDecoder!
+    var encoder: JSONEncoder!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        jsonString = "{\"id\": \"0\", \"private\": true, \"userId\": 2, \"duration\": \"00:20\", \"postDate\": \"2019-01-20 12:43\", \"vlogId\": \"2\"}"
+        decoder = JSONDecoder()
+        encoder = JSONEncoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC+0:00")
+        encoder.dateEncodingStrategy = .formatted(dateFormatter)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        jsonString = nil
+        decoder = nil
+        encoder = nil
     }
     
     func testJSONToVlogReaction() {
-        let jsonString = "{\"id\": \"0\", \"private\": true, \"userId\": 2, \"duration\": \"00:20\", \"postDate\": \"2019-01-20 12:43\", \"vlogId\": \"2\"}"
         let jsonData = jsonString.data(using: .utf8)
-        let decoder = JSONDecoder()
         let vlogReaction = try? decoder.decode(VlogReaction.self, from: jsonData!)
         XCTAssertNotNil(vlogReaction, "The json string does not conform the VlogReaction model")
+    }
+    
+    func testVlogReactionToJSON() {
+        let jsonData = jsonString.data(using: .utf8)
+        let originalJsonDict = try? JSONSerialization.jsonObject(with: jsonData!, options: []) as! [String: AnyHashable]
+        
+        let vlogReaction = try? decoder.decode(VlogReaction.self, from: jsonData!)
+        let vlogReactionToJsonData = try? encoder.encode(vlogReaction)
+        let vlogReactionToJsonDict = try? JSONSerialization.jsonObject(with: vlogReactionToJsonData!, options: []) as! [String: AnyHashable]
+        
+        XCTAssertEqual(originalJsonDict, vlogReactionToJsonDict, "The original json is not equal to the vlog reaction generated json: Original: \(originalJsonDict) | Generated: \(vlogReactionToJsonDict)")
     }
 
 }
