92aa4f9803b232d62263b819b35945f134f94275
James <jamesb@live.nl>
2019-10-24 17:17:11 +0200

    updated

--- Swabbr/Extension/DataFormatter+StringHandling.swift
+++ Swabbr/Extension/DataFormatter+StringHandling.swift
@@ -12,14 +12,14 @@ extension DateFormatter {
     
     /**
      This function is almost the same as the date(from: "stringdate") function but this will set the
-     timezone correctly to UTC+0:00.
+     timezone if it is required for a particular value.
      - parameter format: A string value which represents the value of the incoming date string.
      - parameter value: A string value which represents the actual date string.
      - Returns: A nullable date.
     */
     func stringToBaseDate(format: String, value: String) -> Date? {
         self.dateFormat = format
-        self.timeZone = TimeZone(abbreviation: "UTC+0:00")
+        self.timeZone = TimeZone(abbreviation: "UTC")
         return self.date(from: value)
     }
     
