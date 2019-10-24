23e0f0cf19424d1021ea479caa726252738310e2
James <jamesb@live.nl>
2019-10-24 17:16:39 +0200

    added encode functionality

--- Swabbr/Model/Vlog.swift
+++ Swabbr/Model/Vlog.swift
@@ -56,6 +56,19 @@ class Vlog: Codable {
         
         owner = nil
         ownerId = try container.decode(Int.self, forKey: CodingKeys.ownerId)
+    func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(String(id), forKey: .id)
+        try container.encode(isPrivate, forKey: .isPrivate)
+        try container.encode(ownerId, forKey: .ownerId)
+        try container.encode(duration, forKey: .duration)
+        try container.encode(startDate, forKey: .startDate)
+        try container.encode(totalLikes, forKey: .totalLikes)
+        try container.encode(totalReactions, forKey: .totalReactions)
+        try container.encode(totalViews, forKey: .totalViews)
+        try container.encode(isLive, forKey: .isLive)
         
     }
     
