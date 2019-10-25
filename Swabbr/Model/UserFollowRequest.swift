b0fd87a098db000cf9a22d1fa3d83eb9055b47cb
James <jamesb@live.nl>
2019-10-25 10:37:00 +0200

    added encoding function to follow requests

--- Swabbr/Model/UserFollowRequest.swift
+++ Swabbr/Model/UserFollowRequest.swift
@@ -36,13 +36,25 @@ struct UserFollowRequest: Codable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        id = try container.decodeToType(Int.self, key: CodingKeys.id)
-        requesterId = try container.decodeToType(Int.self, key: CodingKeys.requesterId)
-        receiverId = try container.decodeToType(Int.self, key: CodingKeys.receiverId)
+        id = try container.decodeToType(Int.self, key: .id)
+        requesterId = try container.decodeToType(Int.self, key: .requesterId)
+        receiverId = try container.decodeToType(Int.self, key: .receiverId)
         
-        status = try container.decode(Status.self, forKey: CodingKeys.status)
+        status = try container.decode(Status.self, forKey: .status)
         
-        timestamp = DateFormatter().stringToBaseDate(format: "yyyy-MM-dd HH:mm", value: try container.decode(String.self, forKey: CodingKeys.timestamp))!
+        timestamp = DateFormatter().stringToBaseDate(format: "yyyy-MM-dd HH:mm", value: try container.decode(String.self, forKey: .timestamp))!
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(requesterId, forKey: .requesterId)
+        try container.encode(receiverId, forKey: .receiverId)
+        try container.encode(status, forKey: .status)
+        try container.encode(timestamp, forKey: .timestamp)
+        
     }
     
 }
