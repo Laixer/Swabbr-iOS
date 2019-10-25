e71c46098a878ff0f0dcb914ce3656db0b4fae29
James <jamesb@live.nl>
2019-10-25 10:37:56 +0200

    fixed test errors by resetting app value

--- SwabbrUITests/SwabbrUITests.swift
+++ SwabbrUITests/SwabbrUITests.swift
@@ -27,7 +27,7 @@ class SwabbrUITests: XCTestCase {
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
     func testCurrentUserProfileWithUpdateButton() {
