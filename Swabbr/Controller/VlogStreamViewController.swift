24e6d68237c15d5843745c15eab8da1270e6b436
James <jamesb@live.nl>
2019-11-04 16:11:09 +0100

    updated to new base

--- Swabbr/Controller/VlogStreamViewController.swift
+++ Swabbr/Controller/VlogStreamViewController.swift
@@ -7,37 +7,31 @@
 //
 //  This class will handle all Streaming related actions
 //
-
-import UIKit
-import CoreMedia
-import AVFoundation
-import Photos
 import HaishinKit
 import VideoToolbox
-import NextLevel
+import CoreMedia
+import AVFoundation
 
-class VlogStreamViewController : UIViewController, BaseViewProtocol {
+class VlogStreamViewController: VlogMakerBaseViewController {
     
-    
-    private var previewView: GLHKView!
-    
-    private let controlView = VlogStreamControlView(isStreaming: true)
-    
-    fileprivate var beginZoomScale: Float = 1.0
-    fileprivate var focusView = FocusIndicatorView(frame: .zero)
+    private let streamUrl: String!
     
     private let maxRetry = 5
     private var retryCount = 0
     
-    private var currentPosition: AVCaptureDevice.Position = .back
+    private let rtmpConnection = RTMPConnection()
+    private var rtmpStream: RTMPStream!
     
-    let rtmpConnection = RTMPConnection()
-    var rtmpStream: RTMPStream!
+    init(streamUrl: String) {
+        self.streamUrl = streamUrl
+        super.init(isStreaming: true)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationController?.isNavigationBarHidden = true
         
         rtmpStream = RTMPStream(connection: rtmpConnection)
         
@@ -78,17 +72,33 @@ class VlogStreamViewController : UIViewController, BaseViewProtocol {
         rtmpConnection.addEventListener(.rtmpStatus, selector: #selector(rtmpStatusHandler), observer: self)
         rtmpConnection.addEventListener(.ioError, selector: #selector(ioErrorHandler), observer: self)
         
-        initElements()
-        applyConstraints()
-        
         controlView.startOperateView {
             self.rtmpConnection.connect(self.streamUrl, arguments: nil)
         }
-
-        controlView.recordButton.addTarget(self, action: #selector(recordButtonClicked), for: .touchUpInside)
-        controlView.flipCameraTopLeftButton.addTarget(self, action: #selector(switchButtonClicked), for: .touchUpInside)
-        controlView.flipCameraBottomRightButton.addTarget(self, action: #selector(switchButtonClicked), for: .touchUpInside)
+    }
+    
+    override func prepareForVlog() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setPreferredSampleRate(44_100)
+            
+            if #available(iOS 10.0, *) {
+                try session.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth])
+            } else {
+                session.perform(NSSelectorFromString("setCategory:withOptions:error:"), with: AVAudioSession.Category.playAndRecord, with:  [AVAudioSession.CategoryOptions.allowBluetooth])
+            }
+            try session.setActive(true)
+        } catch {
+        }
         
+        rtmpStream.attachAudio(AVCaptureDevice.default(for: .audio)) { error in
+            print("Error with audio")
+        }
+        rtmpStream.attachCamera(DeviceUtil.device(withPosition: currentPosition)) { error in
+            print("Error with camera")
+        }
+        
+        (previewView as! GLHKView).attachStream(self.rtmpStream)
     }
     
     @objc func rtmpStatusHandler(_ notification: Notification) {
@@ -121,168 +131,17 @@ class VlogStreamViewController : UIViewController, BaseViewProtocol {
         }
     }
     
-    internal func initElements() {
-        
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture(_:)))
-        pinchGesture.delegate = self
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSingleTapGesture(_:)))
-        tapGesture.numberOfTapsRequired = 1
-        tapGesture.delegate = self
-        
-        let screenBounds = UIScreen.main.bounds
-        previewView = GLHKView(frame: screenBounds)
-        previewView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        previewView.addGestureRecognizer(pinchGesture)
-        previewView.addGestureRecognizer(tapGesture)
-        previewView.videoGravity = .resizeAspectFill
-        view.addSubview(previewView)
-        view.addSubview(controlView)
+    override func recordButtonClicked() {
+        rtmpStream.close()
+        rtmpStream.dispose()
     }
     
-    internal func applyConstraints() {
-        controlView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            
-            controlView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            controlView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            controlView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            controlView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-            
-        ])
-    }
-    
-    @objc func recordButtonClicked() {
-        stopStream()
-    }
-    
-    @objc func switchButtonClicked() {
+    override func switchButtonClicked() {
         let position: AVCaptureDevice.Position = currentPosition == .back ? .front : .back
         rtmpStream.attachCamera(DeviceUtil.device(withPosition: position)) { error in
             print(error)
         }
         currentPosition = position
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if NextLevel.authorizationStatus(forMediaType: AVMediaType.video) == .authorized &&
-            NextLevel.authorizationStatus(forMediaType: AVMediaType.audio) == .authorized {
-            prepareStream()
-        } else {
-            NextLevel.requestAuthorization(forMediaType: AVMediaType.video) { (mediaType, status) in
-                print("NextLevel, authorization updated for media \(mediaType) status \(status)")
-                if NextLevel.authorizationStatus(forMediaType: AVMediaType.video) == .authorized &&
-                    NextLevel.authorizationStatus(forMediaType: AVMediaType.audio) == .authorized {
-                    self.prepareStream()
-                } else if status == .notAuthorized {
-                    // gracefully handle when audio/video is not authorized
-                    print("NextLevel doesn't have authorization for audio or video")
-                }
-            }
-            NextLevel.requestAuthorization(forMediaType: AVMediaType.audio) { (mediaType, status) in
-                print("NextLevel, authorization updated for media \(mediaType) status \(status)")
-                if NextLevel.authorizationStatus(forMediaType: AVMediaType.video) == .authorized &&
-                    NextLevel.authorizationStatus(forMediaType: AVMediaType.audio) == .authorized {
-                    self.prepareStream()
-                } else if status == .notAuthorized {
-                    // gracefully handle when audio/video is not authorized
-                    print("NextLevel doesn't have authorization for audio or video")
-                }
-            }
-        }
-    }
-    
-    private func prepareStream() {
-        
-        let session = AVAudioSession.sharedInstance()
-        do {
-            try session.setPreferredSampleRate(44_100)
-            
-            if #available(iOS 10.0, *) {
-                try session.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth])
-            } else {
-                session.perform(NSSelectorFromString("setCategory:withOptions:error:"), with: AVAudioSession.Category.playAndRecord, with:  [AVAudioSession.CategoryOptions.allowBluetooth])
-            }
-            try session.setActive(true)
-        } catch {
-        }
-        
-        rtmpStream.attachAudio(AVCaptureDevice.default(for: .audio)) { error in
-            print("Error with audio")
-        }
-        rtmpStream.attachCamera(DeviceUtil.device(withPosition: currentPosition)) { error in
-            print("Error with camera")
-        }
-        
-        self.previewView.attachStream(self.rtmpStream)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    
-    private func stopStream() {
-        rtmpStream.close()
-        rtmpStream.dispose()
-    }
-    
-}
-
-// MARK: UIGestureRecognizerDelegate
-extension VlogStreamViewController : UIGestureRecognizerDelegate {
-
-    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if gestureRecognizer.isKind(of: UIPinchGestureRecognizer.self) {
-            beginZoomScale = Float(rtmpStream.zoomFactor)
-        }
-        return true
-    }
-    
-    /**
-     This will handle the "Pinch" gesture. The pinch gesture is responsible for the zooming.
-     - parameter pinch: An UIPinchGestureRecognizer object.
-    */
-    @objc fileprivate func handlePinchGesture(_ pinch: UIPinchGestureRecognizer) {
-        beginZoomScale = beginZoomScale * Float(pinch.scale)
-        rtmpStream.setZoomFactor(CGFloat(beginZoomScale))
-    }
-    
-    /**
-     This will handle the "Tap" gesture. The tap gesture will create an area where the camera
-     will put it's focus.
-     - parameter tap: An UITapGestureRecognizer object.
-    */
-    @objc fileprivate func handleSingleTapGesture(_ tap: UITapGestureRecognizer) {
-        
-        let tapPoint = tap.location(in: previewView!)
-        
-        var focusFrame = focusView.frame
-        focusFrame.origin.x = CGFloat((tapPoint.x - (focusFrame.size.width * 0.5)).rounded())
-        focusFrame.origin.y = CGFloat((tapPoint.y - (focusFrame.size.height * 0.5)).rounded())
-        focusView.frame = focusFrame
-        
-        previewView?.addSubview(focusView)
-        focusView.startAnimation()
-        
-        if let captureDevice = DeviceUtil.device(withPosition: currentPosition) {
-            do {
-                try captureDevice.lockForConfiguration()
-                
-                captureDevice.focusPointOfInterest = tapPoint
-                captureDevice.focusMode = .autoFocus
-                captureDevice.exposurePointOfInterest = tapPoint
-                captureDevice.exposureMode = .continuousAutoExposure
-                captureDevice.unlockForConfiguration()
-            } catch {
-                
-            }
-        }
-        
-        rtmpStream.setPointOfInterest(tapPoint, exposure: tapPoint)
-        
     }
     
 }
