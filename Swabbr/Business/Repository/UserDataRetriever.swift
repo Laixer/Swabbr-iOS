66c818a0576e071a0d007be6d5f77217eb172f25
James <jamesb@live.nl>
2019-11-18 10:52:08 +0100

    added data retrievers to business

---
+++ Swabbr/Business/Repository/UserDataRetriever.swift
@@ -0,0 +1,27 @@
+//
+//  UserDataRetriever.swift
+//  Swabbr
+//
+//  Created by James Bal on 14-11-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+class UserDataRetriever: IRepository {
+    
+    typealias Model = UserModel
+    
+    static let shared = UserDataRetriever()
+    
+    private let repository = UserRepository.shared
+    
+    func get(refresh: Bool, completionHandler: @escaping ([UserModel]?) -> Void) {
+        repository.get(refresh: refresh, completionHandler: completionHandler)
+    }
+    
+    func get(id: Int, refresh: Bool, completionHandler: @escaping (UserModel?) -> Void) {
+        repository.get(id: id, refresh: refresh, completionHandler: completionHandler)
+    }
+    
+}
---
+++ Swabbr/Business/Repository/VlogDataRetriever.swift
@@ -0,0 +1,25 @@
+//
+//  VlogDataRetriever.swift
+//  Swabbr
+//
+//  Created by James Bal on 14-11-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+class VlogDataRetriever: IRepository {
+    typealias Model = VlogModel
+    
+    static let shared = VlogDataRetriever()
+    
+    private let repository = VlogRepository.shared
+    
+    func get(refresh: Bool, completionHandler: @escaping ([VlogModel]?) -> Void) {
+        repository.get(refresh: refresh, completionHandler: completionHandler)
+    }
+    
+    func get(id: Int, refresh: Bool, completionHandler: @escaping (VlogModel?) -> Void) {
+        repository.get(id: id, refresh: refresh, completionHandler: completionHandler)
+    }
+}
