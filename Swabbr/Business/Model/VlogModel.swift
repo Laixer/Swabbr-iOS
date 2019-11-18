71189300307758d40b1f7a523fc50f18d11f3347
James <jamesb@live.nl>
2019-11-18 10:42:52 +0100

    added business models

---
+++ Swabbr/Business/Model/VlogModel.swift
@@ -0,0 +1,23 @@
+//
+//  VlogModel.swift
+//  Swabbr
+//
+//  Created by James Bal on 14-11-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+import Foundation
+
+struct VlogModel {
+    
+    var id: Int
+    var isPrivate: Bool
+    var duration: String
+    var startDate: Date
+    var totalLikes: Int
+    var totalReactions: Int
+    var totalViews: Int
+    var isLive: Bool
+    var ownerId: Int
+    
+}
