644f27255922039b278df392aa4f695cbb052eae
James <jamesb@live.nl>
2019-11-19 14:59:36 +0100

    updated datasource protocol

--- Swabbr/Data/Datasource/DataSourceProtocol.swift
+++ Swabbr/Data/Datasource/DataSourceProtocol.swift
@@ -1,14 +1,18 @@
 //
-//  IDataSource.swift
+//  DataSourceProtocol.swift
 //  Swabbr
 //
 //  Created by James Bal on 15-11-19.
 //  Copyright © 2019 Laixer. All rights reserved.
 //
 
-protocol IDataSource {
+protocol DataSourceProtocol {
     associatedtype Entity: Decodable
     
     func get(completionHandler: @escaping ([Entity]?) -> Void)
     func get(id: Int, completionHandler: @escaping (Entity?) -> Void)
+}
+
+protocol DataSourceMultipleProtocol: DataSourceProtocol {
+    func get(id: Int, multiple completionHandler: @escaping ([Entity]?) -> Void)
 }
