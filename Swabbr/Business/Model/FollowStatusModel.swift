4301ffdf2b04bbf00a5b5b21efc240b3b1df22c4
James <jamesb@live.nl>
2019-12-11 15:26:38 +0100

    added follow status and user settings model

---
+++ Swabbr/Business/Model/FollowStatusModel.swift
@@ -0,0 +1,14 @@
+//
+//  FollowStatus.swift
+//  Swabbr
+//
+//  Created by James Bal on 04-12-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+struct FollowStatusModel {
+    
+    let id: String
+    let status: String
+    
+}
---
+++ Swabbr/Business/Model/UserSettingsModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserSettingsModel.swift
+//  Swabbr
+//
+//  Created by James Bal on 04-12-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+struct UserSettingsModel {
+
+    let isPrivate: Bool
+    let dailyVlogRequestLimit: Int
+    let followMode: Int
+    
+}
