5260a7d0c86daac6c81e49050bc943c3ac432d28
James <jamesb@live.nl>
2019-12-12 16:12:34 +0100

    public enum followmode

--- Swabbr/Presentation/Model/UserSettingsItem.swift
+++ Swabbr/Presentation/Model/UserSettingsItem.swift
@@ -18,27 +18,6 @@ struct UserSettingsItem {
         followMode = FollowMode(rawValue: userSettingsModel.followMode)!
     }
     
-    enum FollowMode: Int, CaseIterable {
-        case always = 0, never, manual
-        
-        var stringCode: String {
-            switch self {
-            case .always: return "Always"
-            case .never: return "Never"
-            case .manual: return "Manual"
-            }
-        }
-        
-        var stringCodes: [String] {
-            return FollowMode.allCases.map({$0.stringCode})
-        }
-        
-        var enumCodes: [FollowMode] {
-            return FollowMode.allCases
-        }
-        
-    }
-    
     func mapToBusiness() -> UserSettingsModel {
         return UserSettingsModel(isPrivate: isPrivate, dailyVlogRequestLimit: dailyVlogRequestLimit, followMode: followMode.rawValue)
     }
@@ -55,4 +34,8 @@ extension UserSettingsItem: Equatable {
     static func == (lhs: UserSettingsItem, rhs: UserSettingsItem) -> Bool {
         return lhs.isPrivate == rhs.isPrivate && lhs.dailyVlogRequestLimit == rhs.dailyVlogRequestLimit && lhs.followMode == rhs.followMode
     }
+}
+
+public enum FollowMode: Int, CaseIterable {
+    case always = 0, never, manual
 }
