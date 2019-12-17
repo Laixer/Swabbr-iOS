1d243c21993a5d5276cf353f346fbf16d789355d
James <jamesb@live.nl>
2019-12-17 10:26:08 +0100

    added login and registration user model

---
+++ Swabbr/Business/Model/LoginUserModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginUserModel.swift
+//  Swabbr
+//
+//  Created by James Bal on 13-12-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+struct LoginUserModel {
+    
+    var email: String
+    var password: String
+    
+}
---
+++ Swabbr/Business/Model/RegistrationUserModel.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationUserModel.swift
+//  Swabbr
+//
+//  Created by James Bal on 13-12-19.
+//  Copyright © 2019 Laixer. All rights reserved.
+//
+
+struct RegistrationUserModel {
+    
+    var firstName: String
+    var lastName: String
+    var gender: Int
+    var country: String
+    var email: String
+    var password: String
+    var birthdate: Date
+    var timezone: String
+    var username: String
+    var profileImageUrl: String
+    var isPrivate: Bool
+    var phoneNumber: String
+    
+}
