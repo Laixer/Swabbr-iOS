bf6a4f3752f6bb105e89f9d8e542af4849c28e80
James <jamesb@live.nl>
2020-01-07 09:13:07 +0100

    updated network protocol

--- Swabbr/Data/Datasource/Network/NetworkProtocol.swift
+++ Swabbr/Data/Datasource/Network/NetworkProtocol.swift
@@ -13,13 +13,19 @@ extension NetworkProtocol {
     /**
      This will build up the url with possible queries if required.
      - parameter queryItems: This represents the possible parameters that will be given to an url, default is nil.
-     - Returns: An URL object.
+     - parameter path: Optional string which will redirect to a specific path in the domain.
+     - parameter authorization: Set to true to require the access token to be added, default is false.
+     - Returns: An URLRequest object.
     */
-    internal func buildUrl(queryItems: [URLQueryItem]? = nil, path: String = "") -> URL {
+    internal func buildUrl(queryItems: [URLQueryItem]? = nil, path: String = "", authorization: Bool = false) -> URLRequest {
         var components = URLComponents(string: ApiPreferences.shared.api_url)!
         components.path = ApiPreferences.shared.url_path + "/"  + endPoint
         components.path += "/" + path
         components.queryItems = queryItems
-        return components.url!
+        var urlRequest = URLRequest(url: components.url!)
+        if authorization {
+            urlRequest.addValue(String.init(format: "Bearer {0}", UserDefaults.standard.getAccessToken()!), forHTTPHeaderField: "Authorization")
+        }
+        return urlRequest
     }
 }
